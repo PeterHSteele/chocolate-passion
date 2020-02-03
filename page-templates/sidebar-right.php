7263d3ab85b4b650af4e3fa14c2b55c02a33a045
Peter <steele.peter.3@gmail.com>
2020-02-03 06:56:29 -0500

    directory for page templates

---
+++ page-templates/sidebar-right.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Sidebar Right
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package chocolate_passion
+ */
+
+get_header();
+?>
+	<div class="col-80"> 
+	<div id="primary" class="content-area">
+		
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'sidebar-right' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+?>
+</div><!--.col-80-->
+<?
+get_footer();
