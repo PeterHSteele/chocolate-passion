de0b35461af972b210cea30537c703ea4f865794
Peter <steele.peter.3@gmail.com>
2020-02-04 03:27:56 -0500

    escaped url for bg image & ++ styles for size, position

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -8,30 +8,27 @@
  */
 
 $slides = chocolate_passion_get_slides();
-if ( $slides && !empty($slides) ) :
+if ( !empty($slides) ) :
 ?>
-<section class="slider col-80">
-	<div class="slider-container">
-		<?php foreach( $slides as $slide ): ?>
-		<!--<div class="slide">-->
-		<div class="slide" style="background: url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $slide ), 'full' )[0]; ?>); background-size:cover">
-		<!--</div>-->
-		<?php //echo get_the_post_thumbnail( $slide ); ?>
-		<div class="slide-content">
-			<header class="entry-header">
-				<h2><?php echo get_the_title( $slide ); ?></h2>
-			</header>
+	<section class="slider col-80">
+		<div class="slider-container">
+			<?php 
+			foreach( $slides as $slide ): 
+			$bg_img = wp_get_attachment_image_src( get_post_thumbnail_id( $slide ), 'full' )[0];
+			?>
+			<div class="slide" style="background-image: url(<?php echo esc_attr( $bg_img ); ?>); ">
+			
+			<div class="slide-content">
+				<header class="entry-header">
+					<h2><?php echo get_the_title( $slide ); ?></h2>
+				</header>
 
-			<div class="entry-content">
-				<?php
-				//print_r( esc_html(get_the_content( null, false, $slide )) );
-				echo chocolate_passion_custom_excerpt( get_the_content( null, false, $slide ) );
-				//echo get_the_excerpt( $slide );
-				?>
-			</div>
-		</div><!--.slide-content-->
-		</div><!--.slide-->
-		<?php endforeach; ?>
-	</div>
-</section><!--.slider-section-->
+				<div class="entry-content">
+					<?php echo chocolate_passion_custom_excerpt( get_the_content( null, false, $slide ) ); ?>
+				</div>
+			</div><!--.slide-content-->
+			</div><!--.slide-->
+			<?php endforeach; ?>
+		</div>
+	</section><!--.slider-section-->
 <?php endif;
