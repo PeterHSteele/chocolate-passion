4df16244077f97bd5a852ed0480442e8aa3a68de
Peter <steele.peter.3@gmail.com>
2020-02-09 02:12:19 -0500

    small screen post-thumbnail spacing

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,11 +10,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class='col-60'>
+	
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
-	</div>
+	
 	<?php chocolate_passion_post_thumbnail(); ?>
 	<div class="col-60">
 	<div class="entry-content">
