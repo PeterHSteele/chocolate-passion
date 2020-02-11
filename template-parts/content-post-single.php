fe0d5b455dd52bb32373d8ffb388627385621275
Peter <steele.peter.3@gmail.com>
2020-02-11 07:37:17 -0500

    removed not single callback, added screen reader teaser

--- template-parts/content-post-single.php
+++ template-parts/content-post-single.php
@@ -15,57 +15,52 @@
 		<span class="sticky-icon"><i class="fas fa-lg fa-thumbtack"></i></span>
 	<?php endif; ?>
 	<div class="col-60">
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		<header class="entry-header">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				chocolate_passion_posted_on();
-				//printf( '<span class="single-post-meta-sep">%1$s</span>', esc_html_e(' | ') )
-				chocolate_passion_posted_by();
+			if ( 'post' === get_post_type() ) :
 				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+				<div class="entry-meta">
+					<?php
+					chocolate_passion_posted_on();
+					//printf( '<span class="single-post-meta-sep">%1$s</span>', esc_html_e(' | ') )
+					chocolate_passion_posted_by();
+					?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	
+		
 
-	<div class="entry-content">
-		<?php
-		if ( ! is_single() ){
-			the_excerpt();
-		} else {
-			the_content();
-		}
-		/*the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				/*__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'chocolate-passion' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );*/
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'chocolate-passion' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'chocolate-passion' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'chocolate-passion' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php chocolate_passion_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+		<footer class="entry-footer">
+			<?php chocolate_passion_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
 	</div><!--.col-60-->
 </article><!-- #post-<?php the_ID(); ?> -->
