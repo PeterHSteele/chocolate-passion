914dc2087c27b8879bd34a3501c630772eba8232
Peter <steele.peter.3@gmail.com>
2020-02-18 03:29:15 -0500

    moved sidebar to own template, fixed mini cart link style

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -116,19 +116,6 @@ add_action( 'init' , 'chocolate_passion_rearrange_hooks' );
 add_filter( 'woocommerce_enqueuechocolate_passiontyles', '__return_empty_array' );
 
 /**
- * Add 'woocommerce-active' class to the body tag.
- *
- * @param  array $classes CSS classes applied to the body tag.
- * @return array $classes modified to include 'woocommerce-active' class.
- */
-function chocolate_passion_woocommerce_active_body_class( $classes ) {
-	$classes[] = 'woocommerce-active';
-
-	return $classes;
-}
-add_filter( 'body_class', 'chocolate_passion_woocommerce_active_body_class' );
-
-/**
  * Products per page.
  *
  * @return integer number of products.
@@ -136,27 +123,8 @@ add_filter( 'body_class', 'chocolate_passion_woocommerce_active_body_class' );
 function chocolate_passion_woocommerce_products_per_page() {
 	return 12;
 }
-add_filter( 'loopchocolate_passionhop_per_page', 'chocolate_passion_woocommerce_products_per_page' );
+add_filter( 'loop_shop_per_page', 'chocolate_passion_woocommerce_products_per_page' );
 
-/**
- * Product gallery thumnbail columns.
- *
- * @return integer number of columns.
- */
-function chocolate_passion_woocommerce_thumbnail_columns() {
-	return 4;
-}
-add_filter( 'woocommerce_product_thumbnails_columns', 'chocolate_passion_woocommerce_thumbnail_columns' );
-
-/**
- * Default loop columns on product archives.
- *
- * @return integer products per row.
- */
-function chocolate_passion_woocommerce_loop_columns() {
-	return 3;
-}
-add_filter( 'loopchocolate_passionhop_columns', 'chocolate_passion_woocommerce_loop_columns' );
 
 /**
  * Related Products Args.
@@ -175,31 +143,6 @@ function chocolate_passion_woocommerce_related_products_args( $args ) {
 	return $args;
 }
 add_filter( 'woocommerce_output_related_products_args', 'chocolate_passion_woocommerce_related_products_args' );
-
-if ( ! function_exists( 'chocolate_passion_woocommerce_product_columns_wrapper' ) ) {
-	/**
-	 * Product columns wrapper.
-	 *
-	 * @return  void
-	 */
-	function chocolate_passion_woocommerce_product_columns_wrapper() {
-		$columns = chocolate_passion_woocommerce_loop_columns();
-		echo '<div class="columns-' . absint( $columns ) . '">';
-	}
-}
-add_action( 'woocommerce_before_shop_loop', 'chocolate_passion_woocommerce_product_columns_wrapper', 40 );
-
-if ( ! function_exists( 'chocolate_passion_woocommerce_product_columns_wrapper_close' ) ) {
-	/**
-	 * Product columns wrapper close.
-	 *
-	 * @return  void
-	 */
-	function chocolate_passion_woocommerce_product_columns_wrapper_close() {
-		echo '</div>';
-	}
-}
-add_action( 'woocommerce_after_shop_loop', 'chocolate_passion_woocommerce_product_columns_wrapper_close', 40 );
 
 /**
  * Remove default WooCommerce wrapper.
@@ -248,14 +191,7 @@ add_action( 'woocommerce_after_main_content', 'chocolate_passion_woocommerce_wra
 /**
  * Sample implementation of the WooCommerce Mini Cart.
  *
- * You can add the WooCommerce Mini Cart to header.php like so ...
- *
-	<?php
-		if ( function_exists( 'chocolate_passion_woocommerce_header_cart' ) ) {
-			chocolate_passion_woocommerce_header_cart();
-		}
-	?>
- */
+*/
 
 if ( ! function_exists( 'chocolate_passion_woocommerce_cart_link_fragment' ) ) {
 	/**
@@ -362,11 +298,7 @@ if ( ! function_exists( "chocolate_passion_load_wc_sidebar") ){
 	*/
 
 	function chocolate_passion_load_wc_sidebar(){
-		?>
-		<aside role="secondary" class="woocommerce-sidebar widget-area">
-			<?php dynamic_sidebar( 'sidebar-3' ); ?>
-		</aside>
-		<?php
+		get_sidebar( 'shop' );
 	}
 }
 
---
+++ sidebar-shop.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Woocommerce Sidebar
+*
+* The sidebar that will display on WooCommerce product archive.
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package chocolate_passion
+*/
+?>
+
+<aside role="secondary" class="woocommerce-sidebar widget-area">
+	<?php dynamic_sidebar( 'sidebar-3' ); ?>
+</aside>
