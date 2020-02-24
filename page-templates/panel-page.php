8f72507951ab5ce8c820ed8a47e9542e03c5790e
Peter <steele.peter.3@gmail.com>
2020-02-24 07:15:04 -0500

    separate template for panels

---
+++ page-templates/panel-page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Panel Page
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package chocolate_passion
+ */
+
+get_header();
+
+get_template_part( 'template-parts/panels' );
+
+?>
+
+<div id="primary" class="content-area">
+	<div class="col-80"> 
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!--.col-80-->
+</div><!-- #primary -->
+
+<?php
+get_footer();
